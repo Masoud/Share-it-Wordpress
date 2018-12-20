2d9fad3f7b14d3a31d73962b99d5e3333a4e9f21
masoudnikoomanesh <masoudnikoomanesh@gmail.com>
2018-12-21 00:35:07 +0330

    update: comments for column php file

--- columns.php
+++ columns.php
@@ -1,35 +1,38 @@
 <?php
+
+// Header title of plugin for column
 function sharei_wp_columns_head($defaults) {
     $defaults['first_column']  = 'Share it Wordpress';
     return $defaults;
-  }
-  function sharei_wp_columns_content($column_name, $post_ID) {
-    if ($column_name == 'first_column') {
-      echo '<button wpfc-clear-column="'.$post_ID.'" class="button wpfc-clear-column-action">
-      <span>Clear</span>
-  </button>';
-    }
-  }
+}
 
-  add_action('manage_posts_columns', 'sharei_wp_columns_head');
-  add_action('manage_posts_custom_column', 'sharei_wp_columns_content', 10, 2);
-  if( isset($_POST['id']) ){
-    salam($_POST['id']);
+// Body text of plugin for column
+function sharei_wp_columns_content($column_name, $post_ID) {
+  if ($column_name == 'first_column') {
+    echo '<button sharei-wp-column="'.$post_ID.'" class="button sharei-wp-column-action"><span>Telegram</span></button>';
   }
-  ?>
-  <script src="http://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
+}
+add_action('manage_posts_columns', 'sharei_wp_columns_head');
+add_action('manage_posts_custom_column', 'sharei_wp_columns_content', 10, 2);
 
-  <script>
+// If post request sent, Call Telegram Function
+if( isset($_POST['id']) ){
+  SendPost($_POST['id']);
+}
+?>
+
+<!-- Call Ajax -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+<script>
   jQuery(document).ready(function(){
-          jQuery("button.button.wpfc-clear-column-action:visible").click(function(e){
-            console.log('hi');
-            $.ajax({
-              type: 'post',
-                            data: {id: jQuery(e.currentTarget).attr("wpfc-clear-column")},
-        success: function(response){
-        $('#response').text('name : ' + response);
-        }
+    jQuery("button.button.sharei-wp-column-action:visible").click(function(e){
+      $.ajax({
+        type: 'post',
+          data: {id: jQuery(e.currentTarget).attr("sharei-wp-column")},
+          success: function(response){
+            $('#response').text('name : ' + response);
+          }
+      });
+    });
   });
-  });
-          });
-    </script>
+</script>
